95e7a8ff844e38c9bf195dddf661fd2e8026bcec
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2019-07-19 21:48:32 +0700

    lanjut mas broo

---
+++ app/views/life/index.php
@@ -0,0 +1,44 @@
+<p class="content">
+	Hi! I'm Anugrah, a plumber who learns coding ☕<br><br>
+	Aku ini terinspirasi dari <a href="https://108kb.io" style="text-decoration: underline;">Fariz</a> yang menurutku idenya itu keren sekali. Maka kucontek di halaman ini.
+</p>
+
+<br>
+
+<div class="title">
+	<h1>Living</h1>
+</div>
+<p class="content">Sekarang aku tinggal di <a href="https://en.wikipedia.org/wiki/Cimahi" style="text-decoration: underline;">Cimahi</a>. Kampung halaman yang terlalu kecil untuk jadi kota namun terlalu sesak untuk dibiarkan jadi kota abal-abal semacam administratif. Bisa dikelilingi hanya dengan satu hari. Bercuaca adem, galau kayak baru diputus cinta.</p>
+
+<br>
+
+<div class="title">
+	<h1>Careers</h1>
+</div>
+<p class="content">Aku bekerja untuk <a href="https://www.google.com/maps/place/UPT+Air+Minum+Kota+Cimahi/@-6.8693845,107.5549354,15z/data=!4m2!3m1!1s0x0:0x64708c2d20ba9327?sa=X&ved=2ahUKEwj8g_zCp73hAhXiQ3wKHcD2D2EQ_BIwCnoECA4QCA" style="text-decoration: underline;">UPT Air Minum Kota Cimahi</a>, sebuah unit pelayanan air minum perpipaan yang dibangun oleh pemerintak Kota Cimahi. Aku bertanggungjawab dalam mengolah air baku menjadi air minum sesuai dengan <a href="https://www.mapurna.id/files/SK_Permenkes_492_2010.pdf" style="text-decoration: underline;">standar yang berlaku</a>.</p>
+
+<br>
+
+<div class="title">
+	<h1>Interests</h1>
+</div>
+<p class="content">
+	<b>• Coding</b>
+	<br>
+Aku belajar koding di waktu luangku. Lalu aku mencoba mengaplikasikan hasil pemebelajranku dengan membuat <a href="https://anugrah.club/lab" style="text-decoration: underline;">aplikasi yang berguna untuk tempatku bekerja</a>. Kini, aku sedang mempelajari PHP sebagai subjek utama pembelajaran.</p>
+
+<br>
+
+<p class="content">
+	<b>• Reading</b>
+	<br>
+Aku adalah pembaca buku. Kamu bisa lihat buku apa yang telah dan sedang kubaca di <a href="https://goodreads.com/dekadensiotak" style="text-decoration: underline;">halaman goodreads ini</a>. Meskipun aku mencintai sastra, kini aku lebih memilih membaca buku-buku non-fiksi demi menambah kemampuanku dalam hal produktivitas dan kreativitas.</p>
+
+<br>
+
+<p class="content">
+	<b>• Podcasting</b>
+	<br>
+Hello, newbie programmer. Kamu bisa dengerin podcast yang kuberi nama <a href="https://anugrah.club/podcast" style="text-decoration: underline;">Podcast dekadensiotak</a>. Aku bicara banyak tentang perjalanan pembelajaran kodingku dan pendapat-pendapatku tentang buku yang kubaca. Episode baru hadir di setiap akhir pekan.</p>
+
+<br><br>
