e457edbea9bc4997f6da4b07261132794eaa687c
Anugrah Ramadhan <dialog.anugrah@gmail.com>
2019-07-24 21:53:09 +0700

    rss

--- app/views/templates/homelinkblog.php
+++ app/views/templates/homelinkblog.php
@@ -1,5 +1,5 @@
 <div class="homelinkblog">
 	<p><a href="<?=BASEURL;?>">&lt; home</a></p>
 	<br>
-		<p class="tengah"><a href="<?=BASEURL;?>/blog/tag/cerita">Cerita</a> | <a href="<?=BASEURL;?>/blog/tag/opini">Opini</a> | <a href="<?=BASEURL;?>/blog/list">List</a> | <a href="<?=BASEURL;?>/public/feed/rssblog">RSS</a></p>
+		<p class="tengah"><a href="<?=BASEURL;?>/blog/tag/cerita">Cerita</a> | <a href="<?=BASEURL;?>/blog/tag/opini">Opini</a> | <a href="<?=BASEURL;?>/blog/list">List</a> | <a href="<?=BASEURL;?>/public/feed/rssblog.php">RSS</a></p>
 </div>
